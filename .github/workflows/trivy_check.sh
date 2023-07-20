c6b143d14b81ef1105b0655653c01d4b37dea697
ferrarijefferson <jefferson.ferrari@conjura.com>
2023-07-20 12:18:43 +0200

    sec(trivy): Add trivy for secury scan

---
+++ .github/workflows/trivy_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for f in $(echo $files)
+    do
+        if [[ -f "$f" ]]; then
+            printf "%0.s#" {1..132} && echo
+            realpath $f
+            trivy --exit-code 1 --severity HIGH,CRITICAL fs --scanners $check $f
+            echo $? >> response.txt
+            printf "%0.s#" {1..132} && echo
+        fi
+done
+
+if [[ $(grep 1 ./response.txt | sort -u) -ne 0 ]]; then
+    exit 1
+fi
