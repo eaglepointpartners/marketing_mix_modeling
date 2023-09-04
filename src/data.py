d65790c2eb4908d34790202faf64529776c98585
Anthony Anderson <anthony.anderson@conjura.com>
2023-09-04 09:48:38 +0000

    add controls to benchmark data prep

--- src/data.py
+++ src/data.py
@@ -1,9 +1,54 @@
 import duckdb as db
 import pandas as pd
+import datetime
+import math
+
+from typing import Optional
 
 BENCHMARK_DATA_PATH = '/root/data/daily_benchmark_metrics.parquet'
 
+def data_fold_in_sample(
+        data: pd.DataFrame,
+        fold_number: int,
+        total_folds: int,
+        training_periods: int,
+        date_grain: str,
+        date_field: str
+    ) -> pd.Series:
+    """
+    Use for constructing cross-validation folds, where the in-sample
+    portion contains a continuous time interval of specified length.
+    All of the remaining data is for out-of-sample testing. The output
+    is a boolean series indicating if the data is in-sample (in-fold).
+    """
+
+    assert fold_number > 0 and fold_number <= total_folds
+    assert training_periods < len(data)
+
+    # use days for windowing
+    training_period_days = training_periods if date_grain == 'daily' else training_periods * 7
+
+    # determine in-sample dates for this fold
+    first_date = data[date_field].min()
+    final_date = data[date_field].max()
+    if fold_number < total_folds:
+        shift = math.floor(((final_date - first_date).days - training_period_days) / total_folds)
+        first_training_date = first_date + datetime.timedelta(days=(fold_number-1)*shift)
+        final_training_date = first_training_date + datetime.timedelta(days=training_period_days-1)
+    else:
+        first_training_date = final_date - datetime.timedelta(days=training_period_days-1)
+        final_training_date = final_date
+
+    # return value
+    in_sample = (data[date_field] >= first_training_date) & (data[date_field] <= final_training_date)
+    return in_sample
+
+
 def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') -> pd.DataFrame:
+    """
+    Builds regression data from benchmark data for a specified org.
+    Choose between daily or weekly data grainularity.
+    """
 
     # base table
     benchmark_metrics = db.sql(
@@ -39,7 +84,7 @@ def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') 
     )
 
     group_by_date = 'week_date' if grain == 'weekly' else 'day_date'
- 
+
     # assemble organisation attributes
     attributes = db.sql(
         """
@@ -114,17 +159,17 @@ def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') 
                 case
                     when channel_platform ilike 'google' then
                         case
-                            when channel_grouping ilike '%shopping%' then 'google_shopping'
-                            when channel_grouping ilike '%paid search%' then 'google_search'
-                            when channel_grouping ilike '%display%' then 'google_display'
-                            when channel_grouping ilike '%performance max%' then 'google_pmax'
-                            when channel_grouping ilike '%video%' then 'google_video'
+                            when (channel_grouping ilike '%shopping%') then 'google_shopping'
+                            when (channel_grouping ilike 'paid search%generic') then 'google_search'
+                            when (channel_grouping ilike '%display%') then 'google_display'
+                            when (channel_grouping ilike '%performance max%') then 'google_pmax'
+                            when (channel_grouping ilike '%video%') then 'google_video'
                             else 'other'
                         end
-                    when channel_platform ilike 'facebook' then 'meta'
-                    when channel_platform ilike 'audience network' then 'meta'
-                    when channel_platform ilike 'instagram' then 'meta'
-                    when channel_platform ilike 'messenger' then 'meta'
+                    when (channel_platform ilike 'facebook') then 'meta'
+                    when (channel_platform ilike 'audience network') then 'meta'
+                    when (channel_platform ilike 'instagram') then 'meta'
+                    when (channel_platform ilike 'messenger') then 'meta'
                     else 'other'
                 end as marketing_channel,
                 sessions,
@@ -150,6 +195,36 @@ def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') 
         """
     )
 
+    controls = db.sql(
+        f"""
+        with assign_channel as (
+            select
+                benchmark_organisation_id,
+                {group_by_date},
+                case
+                    when (channel_grouping ilike 'paid search%branded') then 'branded_search'
+                    when (channel_grouping ilike 'direct') then 'direct'
+                    when (channel_grouping ilike 'organic search') then 'organic_search'
+                    when (channel_grouping ilike 'affiliates') then 'affiliate'
+                    else 'other'
+                end as referring_channel,
+                sessions,
+                new_users
+            from benchmark_metrics
+        )
+        pivot assign_channel
+        on referring_channel in (
+                'branded_search',
+                'organic_search',
+                'affiliate',
+                'direct'
+            )
+        using
+            sum(sessions) as clicks,
+            sum(new_users) as new_clicks
+        """
+    )
+
     # put it all together
     regression_data = db.sql(
         f"""
@@ -164,6 +239,11 @@ def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') 
                     'marketing.' + c for c in marketing.columns
                     if c.lower() not in ('benchmark_organisation_id', group_by_date)
                 ])
+            },
+            {', '.join([
+                    'controls.' + c for c in controls.columns
+                    if c.lower() not in ('benchmark_organisation_id', group_by_date)
+                ])
             }
         from attributes
         join demand
@@ -171,6 +251,9 @@ def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') 
         left join marketing
             on attributes.benchmark_organisation_id = marketing.benchmark_organisation_id
             and demand.{group_by_date} = marketing.{group_by_date}
+        left join controls
+            on attributes.benchmark_organisation_id = controls.benchmark_organisation_id
+            and demand.{group_by_date} = controls.{group_by_date}
         order by attributes.benchmark_organisation_id, demand.{group_by_date}
         """
     )
@@ -180,6 +263,7 @@ def build_regression_data_from_benchmark_org(org_id: str, grain: str = 'daily') 
     # remove historical data until there is non-zero cummulative spend
     regression_data = regression_data.set_index(['benchmark_organisation_id',group_by_date]).sort_index()
     regression_data['cumm_spend'] = regression_data.filter(like='_spend').sum(axis=1).groupby(level=0).cumsum()
-    regression_data = regression_data[regression_data.cumm_spend > 0].reset_index()
+    regression_data['cumm_acquisitions'] = regression_data.acquisitions.groupby(level=0).cumsum()
+    regression_data = regression_data[(regression_data.cumm_spend * regression_data.cumm_acquisitions) > 0].reset_index()
 
-    return regression_data.fillna(0.0)
+    return regression_data.drop(columns=['cumm_spend','cumm_acquisitions']).fillna(0.0)
